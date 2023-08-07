7101dadf8c80a4e7af8ec1096c0043bcab386b66
Kevin Karch <kevin.karch@microfocus.com>
2023-08-07 10:51:53 -0400

    changed connection string for tests

--- postgres/src/test.rs
+++ postgres/src/test.rs
@@ -11,9 +11,11 @@ use super::*;
 use crate::binary_copy::{BinaryCopyInWriter, BinaryCopyOutIter};
 use fallible_iterator::FallibleIterator;
 
+const CONNECTION_STRING: &str = "host=engdev2.verticacorp.com port=32780 user=kkarch dbname=verticadb21435";
+
 #[test]
 fn prepare() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     let stmt = client.prepare("SELECT 1::INT, $1::TEXT").unwrap();
     assert_eq!(stmt.params(), &[Type::TEXT]);
@@ -24,7 +26,7 @@ fn prepare() {
 
 #[test]
 fn query_prepared() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     let stmt = client.prepare("SELECT $1::TEXT").unwrap();
     let rows = client.query(&stmt, &[&"hello"]).unwrap();
@@ -34,7 +36,7 @@ fn query_prepared() {
 
 #[test]
 fn query_unprepared() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     let rows = client.query("SELECT $1::TEXT", &[&"hello"]).unwrap();
     assert_eq!(rows.len(), 1);
@@ -43,7 +45,7 @@ fn query_unprepared() {
 
 #[test]
 fn transaction_commit() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id SERIAL PRIMARY KEY)")
@@ -64,7 +66,7 @@ fn transaction_commit() {
 
 #[test]
 fn transaction_rollback() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id SERIAL PRIMARY KEY)")
@@ -84,7 +86,7 @@ fn transaction_rollback() {
 
 #[test]
 fn transaction_drop() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id SERIAL PRIMARY KEY)")
@@ -104,8 +106,8 @@ fn transaction_drop() {
 
 #[test]
 fn transaction_drop_immediate_rollback() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
-    let mut client2 = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
+    let mut client2 = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TABLE IF NOT EXISTS foo (id SERIAL PRIMARY KEY)")
@@ -129,7 +131,7 @@ fn transaction_drop_immediate_rollback() {
 
 #[test]
 fn nested_transactions() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .batch_execute("CREATE TEMPORARY TABLE foo (id INT PRIMARY KEY)")
@@ -180,7 +182,7 @@ fn nested_transactions() {
 
 #[test]
 fn savepoints() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .batch_execute("CREATE TEMPORARY TABLE foo (id INT PRIMARY KEY)")
@@ -231,7 +233,7 @@ fn savepoints() {
 
 #[test]
 fn copy_in() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id INT, name TEXT)")
@@ -254,7 +256,7 @@ fn copy_in() {
 
 #[test]
 fn copy_in_abort() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id INT, name TEXT)")
@@ -273,7 +275,7 @@ fn copy_in_abort() {
 
 #[test]
 fn binary_copy_in() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query("CREATE TEMPORARY TABLE foo (id INT, name TEXT)")
@@ -298,7 +300,7 @@ fn binary_copy_in() {
 
 #[test]
 fn copy_out() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query(
@@ -319,7 +321,7 @@ fn copy_out() {
 
 #[test]
 fn binary_copy_out() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query(
@@ -345,7 +347,7 @@ fn binary_copy_out() {
 
 #[test]
 fn portal() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .simple_query(
@@ -372,7 +374,7 @@ fn portal() {
 
 #[test]
 fn cancel_query() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     let cancel_token = client.cancel_token();
     let cancel_thread = thread::spawn(move || {
@@ -390,7 +392,7 @@ fn cancel_query() {
 
 #[test]
 fn notifications_iter() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .batch_execute(
@@ -410,7 +412,7 @@ fn notifications_iter() {
 
 #[test]
 fn notifications_blocking_iter() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .batch_execute(
@@ -422,7 +424,7 @@ fn notifications_blocking_iter() {
         .unwrap();
 
     thread::spawn(|| {
-        let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+        let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
         thread::sleep(Duration::from_secs(1));
         client
@@ -443,7 +445,7 @@ fn notifications_blocking_iter() {
 
 #[test]
 fn notifications_timeout_iter() {
-    let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
     client
         .batch_execute(
@@ -455,7 +457,7 @@ fn notifications_timeout_iter() {
         .unwrap();
 
     thread::spawn(|| {
-        let mut client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+        let mut client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
 
         thread::sleep(Duration::from_secs(1));
         client
@@ -481,7 +483,7 @@ fn notifications_timeout_iter() {
 #[test]
 fn notice_callback() {
     let (notice_tx, notice_rx) = mpsc::sync_channel(64);
-    let mut client = Config::from_str("host=localhost port=5433 user=postgres")
+    let mut client = Config::from_str(CONNECTION_STRING)
         .unwrap()
         .notice_callback(move |n| notice_tx.send(n).unwrap())
         .connect(NoTls)
@@ -496,7 +498,7 @@ fn notice_callback() {
 
 #[test]
 fn explicit_close() {
-    let client = Client::connect("host=localhost port=5433 user=postgres", NoTls).unwrap();
+    let client = Client::connect(CONNECTION_STRING, NoTls).unwrap();
     client.close().unwrap();
 }
 
